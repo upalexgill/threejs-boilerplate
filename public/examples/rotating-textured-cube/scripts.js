36119fb43caf63ca2ebeb729d70d95d7971e8b89
Alex Gill <alex.gill@pfizer.com>
2023-11-27 13:03:43 +0000

    Update scripts.js

--- public/examples/rotating-textured-cube/scripts.js
+++ public/examples/rotating-textured-cube/scripts.js
@@ -5,9 +5,10 @@ import { OBJLoader } from 'https://cdn.jsdelivr.net/npm/three@0.126.0/examples/j
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 
-const renderer = new THREE.WebGLRenderer();
+const renderer = new THREE.WebGLRenderer({ alpha: true });
 renderer.setPixelRatio(window.devicePixelRatio);
 renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.setClearColor( 0x000000, 0 ); // the default
 
 const controls = new OrbitControls(camera, renderer.domElement);
 controls.enableKeys = false;
