df34d4df4fdd3cb6422c804691a1d359e883ceee
Alex Gill <alex.gill@pfizer.com>
2024-02-10 21:06:57 +0000

    Update express

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-app.set('port', (process.env.PORT || 8080));
+app.set('port', (process.env.PORT || 8081));
 
 app.use(express.static('public'));
 
